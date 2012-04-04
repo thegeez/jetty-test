1e26aec69ce1e20ad1593ebbc35a146ee9a63f96
Gijs Stuurman <gijsstuurman@gmail.com>
2012-04-04 21:41:36 +0200

    flush buffer after write status and head

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jetty-test "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-servlet "1.1.0-beta2" :exclusions [org.eclipse.jetty/jetty-server]]
+                 [ring/ring-servlet "1.1.0-beta2" :exclusions [javax.servlet/servlet-api]]
                  [org.eclipse.jetty/jetty-server "8.1.2.v20120308"]])
